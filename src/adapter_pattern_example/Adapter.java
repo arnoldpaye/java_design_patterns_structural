31aff3dfa840b87dfe2ce4cb9f606e882b805508
Arnold Paye <arnoldpayet@gmail.com>
2022-08-08 07:17:41 -0400

    add Adapter class

---
+++ src/adapter_pattern_example/Adapter.java
@@ -0,0 +1,16 @@
+package adapter_pattern_example;
+
+import org.example.UKCarPriceCalculator;
+
+public class Adapter implements PriceCalculator {
+  UKCarPriceCalculator ukCarPriceCalculator;
+
+  public Adapter(UKCarPriceCalculator ukCarPriceCalculator) {
+    this.ukCarPriceCalculator = ukCarPriceCalculator;
+  }
+
+  @Override
+  public String calculatePrice() {
+    return ukCarPriceCalculator.getPrice() + "GBP";
+  }
+}
--- src/adapter_pattern_example/Main.java
+++ src/adapter_pattern_example/Main.java
@@ -1,5 +1,7 @@
 package adapter_pattern_example;
 
+import org.example.UKCarPriceCalculator;
+
 public class Main {
   public static void main(String[] args) {
     CarPriceCalculator carPriceCalculator = new CarPriceCalculator("Ford", 3);
@@ -7,6 +9,10 @@ public class Main {
 
     TruckPriceCalculator truckPriceCalculator = new TruckPriceCalculator(10, 0);
     printVehiclePrice(truckPriceCalculator);
+
+    UKCarPriceCalculator ukCarPriceCalculator = new UKCarPriceCalculator("BMW", 7);
+    Adapter adapter = new Adapter(ukCarPriceCalculator);
+    printVehiclePrice(adapter);
   }
 
   public static void printVehiclePrice(PriceCalculator calculator) {

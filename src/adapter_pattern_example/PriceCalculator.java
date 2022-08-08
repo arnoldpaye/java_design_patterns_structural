1c45412c03f49f326d3d0f04f221ef9a4debd011
Arnold Paye <arnoldpayet@gmail.com>
2022-08-08 07:08:48 -0400

    implement the adapter pattern

---
+++ src/adapter_pattern_example/PriceCalculator.java
@@ -0,0 +1,5 @@
+package adapter_pattern_example;
+
+public interface PriceCalculator {
+  String calculatePrice();
+}
--- src/adapter_pattern_example/TruckPriceCalculator.java
+++ src/adapter_pattern_example/TruckPriceCalculator.java
@@ -1,4 +1,19 @@
 package adapter_pattern_example;
 
-public class TruckPriceCalculator {
+public class TruckPriceCalculator implements PriceCalculator {
+  private int age;
+  private int mileage;
+  public static int averagePrice = 10000;
+
+  public TruckPriceCalculator(int age, int mileage) {
+    this.age = age;
+    this.mileage = mileage;
+  }
+
+
+  @Override
+  public String calculatePrice() {
+    int price = Math.max(averagePrice - age*100 - mileage/100, 0);
+    return price + "USD";
+  }
 }

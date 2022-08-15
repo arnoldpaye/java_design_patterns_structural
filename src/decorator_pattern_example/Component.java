ef2312e49a906f64dfd10f89e664c41f263069a1
Arnold Paye <arnoldpayet@gmail.com>
2022-08-15 07:00:36 -0400

    add decorator_pattern_example package.

---
+++ src/decorator_pattern_example/Component.java
@@ -0,0 +1,7 @@
+package decorator_pattern_example;
+
+import java.awt.Graphics;
+
+public interface Component {
+  void draw(Graphics graphics);
+}
---
+++ src/decorator_pattern_example/ComponentWithRedBorder.java
@@ -0,0 +1,18 @@
+package decorator_pattern_example;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class ComponentWithRedBorder implements Component {
+  Component decoratedComponent;
+
+  public ComponentWithRedBorder(Component component) {
+    decoratedComponent = component;
+  }
+
+  public void draw(Graphics graphics) {
+    graphics.setColor(Color.RED);
+    decoratedComponent.draw(graphics);
+    graphics.setColor(Color.BLACK);
+  }
+}

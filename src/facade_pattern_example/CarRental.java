ae4f3d00c5b7030c3706fc45e3216fe528687fed
Arnold Paye <arnoldpayet@gmail.com>
2022-08-16 06:43:33 -0400

    add facade_pattern_example package.

---
+++ src/facade_pattern_example/CarRental.java
@@ -0,0 +1,9 @@
+package facade_pattern_example;
+
+import java.time.LocalDate;
+
+public class CarRental {
+  public void book(LocalDate start, LocalDate end) {
+    System.out.println("Booking car for " + start + " to " + end);
+  }
+}
---
+++ src/facade_pattern_example/Hotel.java
@@ -0,0 +1,9 @@
+package facade_pattern_example;
+
+import java.time.LocalDate;
+
+public class Hotel {
+  public void book(LocalDate checkinDate, LocalDate checkoutDate) {
+    System.out.println("Booking hotel for " + checkinDate + " to " + checkoutDate);
+  }
+}

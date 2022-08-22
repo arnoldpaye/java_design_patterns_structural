e849ded8b7cbe2d9f08b1659205917f1fe37becc
Arnold Paye <arnoldpayet@gmail.com>
2022-08-22 07:18:38 -0400

    add proxy_pattern_exercise package.

---
+++ src/proxy_pattern_exercise/Inventory.java
@@ -0,0 +1,9 @@
+package proxy_pattern_exercise;
+
+import java.util.ArrayList;
+
+public interface Inventory {
+
+  ArrayList<Item> getInventory();
+
+}
---
+++ src/proxy_pattern_exercise/Main.java
@@ -0,0 +1,17 @@
+package proxy_pattern_exercise;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    Inventory inventory = new SuperstoreInventory();
+    Store store = new Store("Healthy Wholefoods", "Los Angeles", inventory);
+
+    store.printName();
+    store.printLocation();
+    store.printInventory();
+
+
+  }
+
+}
